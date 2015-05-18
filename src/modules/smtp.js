b0a15c781fadcba785527c0e665206afc5361eba
Ivan Radulovic <iradulovic@boardreader.com>
2015-05-18 15:11:09 -0400

    Add port & timeout to SMTP module

--- src/modules/smtp.js
+++ src/modules/smtp.js
@@ -1,5 +1,7 @@
 /***** < ivan *****/
-exports.create = function(usn, psw, host) {
-    return phantom.createSMTP(usn, psw, host);
+exports.create = function(usn, psw, host, port, timeout) {
+	port = port ||  465;
+	timeout = timeout || 30000;
+    return phantom.createSMTP(usn, psw, host, port, timeout);
 };
 /***** ivan > *****/
